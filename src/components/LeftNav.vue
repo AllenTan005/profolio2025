ecaff69e626a18f949575b86ae27bd0911c32bc3
allen-tan <allen_tan@trendmicro.com>
2025-06-10 10:47:51 +0800

    [rwd] 0610 adjust navbar gap

--- src/components/LeftNav.vue
+++ src/components/LeftNav.vue
@@ -1,7 +1,7 @@
 <template>
         <div class=" flex flex-col items-center border-r border-slate-600  w-[10%]  fixed top-0 left-0 h-screen
         @max-[50rem]/main:flex-row @max-[50rem]/main:items-center  @max-[50rem]/main:border-r-0 @max-[50rem]/main:w-[100%] @max-[50rem]/main:h-auto
-           @max-[50rem]/main:p-3
+           @max-[50rem]/main:p-3  @max-[50rem]/main:justify-between 
         ">
             <img src="../assets/logo-allen.svg" alt="logo" class="mb-5 mt-5 w-[30%] 
              @max-[50rem]/main:w-[5%]   @max-[50rem]/main:mt-0     @max-[50rem]/main:mr-5" />
@@ -9,7 +9,7 @@
                 <div 
                 :class="pageStore.pageId === 'home' ? 'bg-[#FFFBEB] rounded-[20px] text-[#BFDBFE]' : ''"
                 class="flex flex-col items-center justify-center mb-5 py-2 px-4 cursor-pointer
-                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]"
+                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]  @max-[50rem]/main:mb-0"
                  @click="pageStore.clickPage('home')">
                  
                     <p class="text font-koulen">Home</p>
@@ -17,7 +17,7 @@
                 <div
                  :class="pageStore.pageId === 'about' ? 'bg-[#BFDBFE] rounded-[20px] text-[#FFFBEB]' : ''"
                 class="flex flex-col items-center justify-center mb-5 py-2 px-4 cursor-pointer
-                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]"
+                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]  @max-[50rem]/main:mb-0"
                     @click="pageStore.clickPage('about')"
                  >
                     <p class="text font-koulen">About</p>
@@ -25,13 +25,13 @@
                 <div
                 :class="pageStore.pageId === 'work' ? 'bg-[#BFDBFE] rounded-[20px] text-[#FFFBEB]' : ''"
                 @click="pageStore.clickPage('work')" class="flex flex-col items-center justify-center mb-5 py-2 px-4 cursor-pointer
-                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]">
+                 hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]  @max-[50rem]/main:mb-0">
                     <p class="text font-koulen">Work</p>
                 </div>
                  <div  
                    :class="pageStore.pageId === 'contact' ? 'bg-[#FFFBEB] rounded-[20px] text-[#BFDBFE] ' : ''"
                  @click="pageStore.clickPage('contact')" class="flex flex-col items-center justify-center mb-5 
-                  py-2 px-4 cursor-pointer hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]">
+                  py-2 px-4 cursor-pointer hover:bg-[#FECACA] hover:text-[#FFFBEB] hover:rounded-[20px]  @max-[50rem]/main:mb-0">
                     <p class="text font-koulen">Contact</p>
                 </div>
             </div>
